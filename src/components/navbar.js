847b47aa187ffaec2f3de89e12cd7ec1a34a320f
Thredas <panteleeva28@gmail.com>
2020-09-05 13:03:11 +0300

    -0.11

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,11 +21,7 @@ export default function Navbar () {
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <Search />
                 <Link to='/create' className='btn btn-warning ml-3 text-dark' href='/'>
-                    <svg style={{'marginTop': '-3px'}} width="1.5em" height="1.5em" viewBox="0 0 16 16"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd"
-                              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                    </svg>
+                    Add
                 </Link>
             </div>
         </nav>
